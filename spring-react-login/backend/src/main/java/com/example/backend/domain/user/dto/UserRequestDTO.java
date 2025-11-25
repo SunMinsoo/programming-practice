61db8b58d0e9f5363bca8af1a5fcbd396ae9a660
SunM <coex0113@gmail.com>
2025-11-25 14:43:25 +0900

    Feat. 10강 User API and login

--- spring-react-login/backend/src/main/java/com/example/backend/domain/user/dto/UserRequestDTO.java
+++ spring-react-login/backend/src/main/java/com/example/backend/domain/user/dto/UserRequestDTO.java
@@ -1,5 +1,8 @@
 package com.example.backend.domain.user.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,8 +10,18 @@ import lombok.Setter;
 @Setter
 public class UserRequestDTO {
 
+    public interface existGroup {}      // 회원 가입 시 username 존재 확인
+    public interface addGroup {}        // 회원 가입 시
+    public interface passwordGroup {}   // 비밀 번호 변경 시
+    public interface updateGroup {}     // 회원 정보 변경 시
+    public interface deleteGroup {}     // 회원 삭제 시
+
+    @NotBlank(groups = {existGroup.class, addGroup.class, updateGroup.class, deleteGroup.class}) @Size(min = 4)
     private String username;
+    @NotBlank(groups = {addGroup.class, passwordGroup.class}) @Size(min = 4)
     private String password;
+    @NotBlank(groups = {addGroup.class, updateGroup.class})
     private String nickname;
+    @Email(groups = {addGroup.class, updateGroup.class})
     private String email;
 }
